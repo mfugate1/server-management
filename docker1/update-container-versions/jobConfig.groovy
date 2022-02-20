7e97e31aabde75de0415702b2c8172c8af318942
Mike Fugate <mfugate@carnegielearning.com>
2022-02-20 11:12:53 -0500

    move all docker1 specific stuff to a separate directory

--- docker1/update-container-versions/jobConfig.groovy
+++ docker1/update-container-versions/jobConfig.groovy
@@ -13,7 +13,7 @@ pipelineJob ('Update-Docker1-Versions') {
                     branch('main')
                 }
                 lightweight()
-                scriptPath('docker/update-job/Jenkinsfile')
+                scriptPath('docker1/update-container-versions/Jenkinsfile')
             }
         }
     }
