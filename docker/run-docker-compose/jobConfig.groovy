3f68191ac44106e608dde337ade43f0f7a4ecbee
Mike Fugate <mfugate@carnegielearning.com>
2022-02-20 10:33:40 -0500

    get docker ip from credentials provider

--- docker/run-docker-compose/jobConfig.groovy
+++ docker/run-docker-compose/jobConfig.groovy
@@ -1,19 +1,13 @@
+def docker1Ip = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
+    org.jenkinsci.plugins.azurekeyvaultplugin.credentials.string.AzureSecretStringCredentials.class, 
+    Jenkins.instance, 
+    null, 
+    null).find{it.id == "DOCKER1-IP"}.getSecret()
+
 job ("Run-Docker-Compose") {
     label("built-in")
-    wrappers {
-        sshAgent("jenkins-ssh")
-        withAzureKeyvault {
-            azureKeyVaultSecrets {
-                azureKeyVaultSecret {
-                    envVariable("DOCKER1_IP")
-                    name("DOCKER1-IP")
-                    secretType("Secret")
-                }
-            }
-        }
-    }
     steps {
-        remoteShell("jenkins@${DOCKER1_IP}:22") {
+        remoteShell("jenkins@${docker1Ip}:22") {
             command([
                 "cd /docker/server-management/docker",
                 "git pull",
