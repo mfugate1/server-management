433a6e46b68e1159892a7bd55d59410451683aa4
Mike Fugate <mfugate@carnegielearning.com>
2022-05-22 16:45:56 -0400

    remove old raspi setup script

--- raspi-docker/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# This enables memory stats in docker that can be monitored in home assistant
-echo "$(cat /boot/cmdline.txt | tr '\n' ' ')cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory" > /boot/cmdline.txt
