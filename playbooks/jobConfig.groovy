d98339fa90930f154e01cf871e303549b8a5d670
Mike Fugate <mfugate@carnegielearning.com>
2022-06-11 10:14:06 -0400

    add job to run any playbook

--- playbooks/jobConfig.groovy
+++ playbooks/jobConfig.groovy
@@ -1,3 +1,6 @@
+import hudson.FilePath
+import hudson.model.Executor
+
 freeStyleJob("Playbook--Apt-Update") {
     scm {
         github("mfugate1/server-management", "refs/heads/main")
@@ -17,6 +20,27 @@ freeStyleJob("Playbook--Apt-Update") {
             }
             credentialsId("jenkins-ssh")
             disableHostKeyChecking(true)
+        }
+    }
+}
+
+String playbookScript = '''\
+    ansiblePlaybook credentialsId: 'jenkins-ssh', disableHostKeyChecking: true, inventory: 'hosts', playbook: playbook
+'''.stripIndent()
+
+FilePath workspace = Executor.currentExecutor().getCurrentWorkspace().child("server-management/playbooks/")
+
+pipelineJob("Run-Ansible-Playbook") {
+    definition {
+        cps {
+            script(playbookScript)
+            sandbox(true)
+        }
+    }
+    parameters {
+        choice {
+            name("playbook")
+            choices(workspace.list().findAll{it.endsWith(".yml")})
         }
     }
 }
